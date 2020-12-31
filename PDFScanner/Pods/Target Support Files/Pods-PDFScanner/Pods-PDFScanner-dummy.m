14b1f95768e8433eb45517a2b2a92701f8da6e8e
jiahongling <m15116996950@163.com>
2020-12-31 12:00:49 +0800

    删除代码

--- PDFScanner/Pods/Target Support Files/Pods-PDFScanner/Pods-PDFScanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PDFScanner : NSObject
-@end
-@implementation PodsDummy_Pods_PDFScanner
-@end
--- PDFScanner/Pods/Target Support Files/loady/loady-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_loady : NSObject
-@end
-@implementation PodsDummy_loady
-@end
